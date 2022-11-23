ab7c548ce46145eb85495d5d3084220fd1587e0c
soncreux <dimakarnur@gmail.com>
2022-11-23 21:43:54 +0300

    okokok

--- ctf/task/admin.py
+++ ctf/task/admin.py
@@ -2,15 +2,4 @@ from django.contrib import admin
 
 from task.models import Post
 
-class PostAdmin(admin.ModelAdmin):
-    
-    list_display = ('id', 'title')
-    readonly_fields = ('thumbnail_preview',)
 
-    def thumbnail_preview(self, obj):
-        return obj.thumbnail_preview
-
-    thumbnail_preview.short_description = 'Thumbnail Preview'
-    thumbnail_preview.allow_tags = True
-
-admin.site.register(Post, PostAdmin)
